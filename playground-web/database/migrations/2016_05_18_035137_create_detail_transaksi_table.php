3a2056e53252af024f1afd32ddb6c111aa1d84d4
Salman Rameli <salmanrameli@hotmail.com>
2016-05-18 11:30:06 +0700

    add column to each table, but without foreign key(s) at all

---
+++ playground-web/database/migrations/2016_05_18_035137_create_detail_transaksi_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDetailTransaksiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_transaksi', function (Blueprint $table) {
+            //perlu dirubah ke foreign key
+            $table->increments('id_transaksi');
+
+            $table->integer('harga_jual');
+            $table->integer('jumlah');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('detail_transaksi');
+    }
+}
---
+++ playground-web/database/migrations/2016_05_18_035316_create_petugas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePetugasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('petugas', function (Blueprint $table) {
+            $table->integer('id_petugas');
+            $table->primary('id_petugas');
+            
+            $table->string('nama_petugas');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('petugas');
+    }
+}
